9ec3a6716c0c4441425a9e16931f85d825ffe649
Giovane <giovane.magri@gmail.com>
2024-10-28 22:56:12 -0300

    função obter arestas

--- IC.py
+++ IC.py
@@ -4,18 +4,18 @@ nos = [1 ,2 ,3 ,4 ,5 ,6 ]
 arestas = [(5, 4, 5), (5, 3, 10), (5, 6, 4), (4, 3, 2), (4, 1, 2)
            , (6, 4, 1), (6, 1, 5), (3, 1, 1), (3, 2, 3), (1, 2, 2)]
 
-def Dijkstra():
+def obter_arestas(arestas, ultimo_rotulo):
+    index = 0
+    arestas_sep = []
 
-    rotulo = []
-    not_rotulo = []
-    origem = int(input("Digite o nó de origem: "))
-    destino = int(input("Digite o nó de destino: "))
-    distancia = 0
-    rotulo.append(origem)
+    for u, v, distancia in arestas:
+        if (u == ultimo_rotulo):
+            arestas_sep.append(arestas[index])
+            index += 1
+        else:
+            index += 1
 
-    gera_rotulo(origem, rotulo, not_rotulo)
-
-    return gera_rotulo
+    return arestas_sep
 
 def gera_rotulo(origem, rotulo, not_rotulo):
 
@@ -26,7 +26,28 @@ def gera_rotulo(origem, rotulo, not_rotulo):
         else:
             not_rotulo.append(i)
     
-    return print(rotulo, not_rotulo)
+    return rotulo, not_rotulo
+
+def Dijkstra():
+
+    rotulo = []
+    not_rotulo = []
+    origem = int(input("Digite o nó de origem: "))
+    destino = int(input("Digite o nó de destino: "))
+    distancia = 0
+    predecessor = 0
+    ultimo_rotulo = origem
+    rotulo.append(origem)
+
+    gera_rotulo(origem, rotulo, not_rotulo)
+
+    minimos = []
+    while ultimo_rotulo != destino:
+        
+        for j in not_rotulo:
+            x = min(distancia, distancia + arestas[ultimo_rotulo,j])
+
+    return gera_rotulo
 
 Dijkstra()
 
